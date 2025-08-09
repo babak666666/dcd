298b56c818729fef917ee969c6644315c995e5e7
unknown <babak333333@gmail.com>
2025-08-09 20:50:50 +0100

    Add decrement function

--- Counter.sol
+++ Counter.sol
@@ -2,3 +2,4 @@
 pragma solidity ^0.8.0;
 contract Counter { uint public count; }
 function increment() public { count += 1; }
+function decrement() public { require(count > 0, 'Count cannot be negative'); count -= 1; }
