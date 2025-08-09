267a2b8b1945e2ff00fd70f2d4dca43931b38383
unknown <babak333333@gmail.com>
2025-08-09 20:51:22 +0100

    Add reset function

--- Counter.sol
+++ Counter.sol
@@ -3,3 +3,4 @@ pragma solidity ^0.8.0;
 contract Counter { uint public count; }
 function increment() public { count += 1; }
 function decrement() public { require(count > 0, 'Count cannot be negative'); count -= 1; }
+function reset() public { count = 0; }
